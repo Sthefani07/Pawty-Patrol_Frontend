6eb986df8039dcfed90a7d7dd779897e1b4f6ed8
Sthefani Denhof <sdenhof7@gmail.com>
2024-11-21 15:40:45 -0800

    update

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -3,8 +3,11 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <Link to='/auth'>Login</Link>
-      <Link to='/home'>Home</Link>
+      <Link to="/">Home</Link>
+      <Link to="/auth">Login</Link>
+      <Link to="/locations">Events</Link>
+      <Link to="/add-event">Create Event</Link>
+      <Link to="/dashboard">Dashboard</Link>
     </nav>
   );
 };
--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import LoginForm from '../components/forms/LoginForm.jsx';
-import SignUp from '../components/forms/SingUp.jsx';
+import SignUp from '../components/forms/SignUp.jsx';
 
 const Auth = () => {
   const [newUser, setNewUser] = useState(false);
